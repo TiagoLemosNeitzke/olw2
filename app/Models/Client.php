3145d264f7a31ef5069b09a963f89cd5cb8c6248
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-02-08 17:59:06 -0400

    [feat:models] Criado as models e feito os relacionamentos das tabelas

---
+++ app/Models/Client.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'address_id',
+        'user-id'
+    ];
+
+    public function address(): HasOne
+    {
+        return $this->hasOne(Address::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sales(): HasMany
+    {
+        return $this->hasMany(Sale::class);
+    }
+}
